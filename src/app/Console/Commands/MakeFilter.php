8b51a23959ec1bedd9e10a19444147a7c2bbc627
blackwhaledev <alizade.t488@gmail.com>
2022-02-12 19:10:21 +0300

    update command

--- src/app/Console/Commands/MakeFilter.php
+++ src/app/Console/Commands/MakeFilter.php
@@ -20,27 +20,30 @@ class MakeFilter extends Command
      * @var string
      */
     protected $description = 'Create    a   new     filter.';
-    public $filter_name;
-    public $type;
-    public $second;
-    public $relation;
-    public $first;
-
     /**
-     * Create a new command instance.
-     *
-     * @return void
+     * @var string
      */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->filter_name = $this->argument('filter');
-        $this->type = $this->option('type');
-        $this->second = $this->option('second');
-        $this->relation = $this->option('relation');
-        $this->first = $this->argument('first');
-    }
+    public $filter_name;
+    /**
+     * @var string
+     */
+    public $type;
+    /**
+     * @var string
+     */
+    public $second;
+    /**
+     * @var string
+     */
+    public $relation;
+    /**
+     * @var string
+     */
+    public $first;
+    /**
+     * @var string
+     */
+    public $content;
 
     /**
      * Execute the console command.
@@ -49,46 +52,44 @@ class MakeFilter extends Command
      */
     public function handle()
     {
+        $this->filter_name = $this->argument('filter');
+        $this->type = $this->option('type');
+        $this->second = $this->option('second');
+        $this->relation = $this->option('relation');
+        $this->first = $this->argument('first');
+        $this->generateClass();
+    }
 
+    private function generateClass()
+    {
         switch ($this->type) {
             case 'between':
-                $this->makeTextFilter();
+
                 break;
             case 'like':
-                $this->makeSelectFilter();
+
                 break;
             case 'relation':
-                $this->makeDateFilter();
+
                 break;
             default:
-                $this->makeTextFilter();
+
                 break;
         }
 
-        if ($this->type === 'between') {
-            if ($this->second == null || $this->first == null) {
-                return $this->error("You should pass first and second options!");
-            } else {
-                $apply_type = $this->type;
+        $this->generateFile();
 
-            }
-        }
-        if ($this->type === 'like') {
-            $apply_type = $this->type;
-        }
-        if ($this->type === 'relation') {
-            if ($this->relation === null) {
-                return $this->error("in relation type you should insert --relation=relationName");
-            }
-            $apply_type = $this->type;
+    }
 
-        }
-
-
+    /**
+     *
+     */
+    private function generateFile()
+    {
         $file = "{$this->classNaming($this->type, $this->filter_name, $this->second)}.php";
         $path = app_path();
-        $file = $path."/QueryFilters/$file";
-        $composerDir = $path."/QueryFilters";
+        $file = $path.DIRECTORY_SEPARATOR."QueryFilters".DIRECTORY_SEPARATOR.$file;
+        $composerDir = $path.DIRECTORY_SEPARATOR."QueryFilters";
 
         if (!is_dir($composerDir)) {
             mkdir($composerDir, 0775);
@@ -96,20 +97,8 @@ class MakeFilter extends Command
         if (file_exists($file)) {
             return $this->error("$file already exist!");
         } else {
-            file_put_contents($file, $contents);
+            file_put_contents($file, 'tetst'/*$this->content*/);
             $this->info("$this->filter_name generated!");
         }
-
-    }
-
-
-    public function classNaming($apply_type, $filter_name, $second)
-    {
-        if ($apply_type == null) {
-            return $filter_name;
-        } else {
-            return strtolower($apply_type).$filter_name.$second;
-        }
-
     }
 }
